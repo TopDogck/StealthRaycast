d4caba1d215b7ddeb634ef1e732c167dabc7743d
Connor <connor2.kearney@live.uwe.ac.uk>
2021-10-16 16:01:39 +0100

    Edge ray

--- Stealth/Assets/Scripts/FOV.cs
+++ Stealth/Assets/Scripts/FOV.cs
@@ -11,16 +11,17 @@ public class FOV : MonoBehaviour
 	public LayerMask targetMask;
 	public LayerMask obstacleMask;
 
-
-	//[HideInInspector]
+	[HideInInspector]
 	public List<Transform> visibleTargets = new List<Transform>();
 
 	public float meshResolution;
-	//public int edgeResolveIterations;
-	//public float edgeDstThreshold;
+	public int edgeResolveIterations;
+	public float edgeDstThreshold;
 
 	public MeshFilter viewMeshFilter;
 	Mesh viewMesh;
+
+	public Material newcolor;
 
 	void Start()
 	{
@@ -31,7 +32,6 @@ public class FOV : MonoBehaviour
 		StartCoroutine("FindTargetsWithDelay", .2f);
 	}
 
-
 	IEnumerator FindTargetsWithDelay(float delay)
 	{
 		while (true)
@@ -40,7 +40,7 @@ public class FOV : MonoBehaviour
 			FindVisibleTargets();
 		}
 	}
-	
+
 	void LateUpdate()
 	{
 		DrawFieldOfView();
@@ -58,10 +58,10 @@ public class FOV : MonoBehaviour
 			if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
 			{
 				float dstToTarget = Vector3.Distance(transform.position, target.position);
-
 				if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
 				{
 					visibleTargets.Add(target);
+					target.GetComponent<MeshRenderer>().material = newcolor;
 				}
 			}
 		}
@@ -72,18 +72,38 @@ public class FOV : MonoBehaviour
 		int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
 		float stepAngleSize = viewAngle / stepCount;
 		List<Vector3> viewPoints = new List<Vector3>();
+		ViewCastInfo oldViewCast = new ViewCastInfo();
 		for (int i = 0; i <= stepCount; i++)
 		{
 			float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
 			ViewCastInfo newViewCast = ViewCast(angle);
+			//Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius, Color.black);
+			if (i > 0)
+			{
+				bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+				if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDstThresholdExceeded))
+				{
+					EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+					if (edge.pointA != Vector3.zero)
+					{
+						viewPoints.Add(edge.pointA);
+					}
+					if (edge.pointB != Vector3.zero)
+					{
+						viewPoints.Add(edge.pointB);
+					}
+				}
+
+			}
 			viewPoints.Add(newViewCast.point);
-			//Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius,Color.red);
+			oldViewCast = newViewCast;
 		}
 
 		int vertexCount = viewPoints.Count + 1;
 		Vector3[] vertices = new Vector3[vertexCount];
 		int[] triangles = new int[(vertexCount - 2) * 3];
 		vertices[0] = Vector3.zero;
+
 		for (int i = 0; i < vertexCount - 1; i++)
 		{
 			vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
@@ -95,18 +115,47 @@ public class FOV : MonoBehaviour
 				triangles[i * 3 + 2] = i + 2;
 			}
 		}
-		viewMesh.Clear();
 
+		viewMesh.Clear();
 		viewMesh.vertices = vertices;
 		viewMesh.triangles = triangles;
 		viewMesh.RecalculateNormals();
 	}
 
+
+	EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+	{
+		float minAngle = minViewCast.angle;
+		float maxAngle = maxViewCast.angle;
+		Vector3 minPoint = Vector3.zero;
+		Vector3 maxPoint = Vector3.zero;
+
+		for (int i = 0; i < edgeResolveIterations; i++)
+		{
+			float angle = (minAngle + maxAngle) / 2;
+			ViewCastInfo newViewCast = ViewCast(angle);
+
+			bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+			if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded)
+			{
+				minAngle = angle;
+				minPoint = newViewCast.point;
+			}
+			else
+			{
+				maxAngle = angle;
+				maxPoint = newViewCast.point;
+			}
+		}
+		return new EdgeInfo(minPoint, maxPoint);
+	}
+
+
 	ViewCastInfo ViewCast(float globalAngle)
 	{
 		Vector3 dir = DirFromAngle(globalAngle, true);
 		RaycastHit hit;
-
+		 
 		if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
 		{
 			return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
@@ -139,6 +188,18 @@ public class FOV : MonoBehaviour
 			point = _point;
 			dst = _dst;
 			angle = _angle;
+		}
+	}
+
+	public struct EdgeInfo
+	{
+		public Vector3 pointA;
+		public Vector3 pointB;
+
+		public EdgeInfo(Vector3 _pointA, Vector3 _pointB)
+		{
+			pointA = _pointA;
+			pointB = _pointB;
 		}
 	}
 }
